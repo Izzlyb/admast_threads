bdcfd33ef7a1b974d4a68e058cb614df61e746f0
Isidro J. Leonyork <ijleon@hotmail.com>
2023-09-29 17:16:16 -0400

    Change result of fetchPosts() to redirect if no result are found.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,22 +2,28 @@ import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
 import ThreadCard from "@/components/cards/ThreadCard"
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
+
+  const fetchResult = await fetchPosts(1, 30);
   const user = await currentUser();
+
+  if(!fetchResult) {
+    redirect("/");
+  }
 
   return (
     <>
       <h1 className="head-text text-left"> Home </h1>
 
       <section className="mt-9 flex flex-col gap-10">
-        {result.posts.length === 0 ? (
+        {fetchResult.posts.length === 0 ? (
           <p className="no-result"> No Threads Found! </p>
         ) : (
           <>
-          {result.posts.map((post) => (
+          {fetchResult.posts.map((post) => (
             <ThreadCard 
                 key={post._id}
                 id={post._id}
